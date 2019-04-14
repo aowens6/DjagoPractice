39cd1526b637c5520b5cff08202402311274a307
aowens6 <austyn.owens1@gmail.com>
2019-04-13 22:38:43 -0500

    Initial commit

---
+++ users/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
+from django.contrib.auth.decorators import login_required
+
+def register(request):
+  if request.method == 'POST':
+      form = UserRegisterForm(request.POST)
+      if form.is_valid():
+        form.save()
+        username = form.cleaned_data.get('username')
+        messages.success(request, f'Your account has been created. You may log in now.')
+        return redirect('login')
+  else:
+    form = UserRegisterForm()
+  return render(request, 'users/register.html', {'form': form})
+
+
+@login_required()
+def profile(request):
+  if request.method == 'POST':
+    uForm = UserUpdateForm(request.POST, instance=request.user)
+    pForm = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+    if uForm.is_valid() and pForm.is_valid():
+      uForm.save()
+      pForm.save()
+      messages.success(request, f'Your account has been updated')
+      return redirect('profile')
+  else:
+    uForm = UserUpdateForm(instance=request.user)
+    pForm = ProfileUpdateForm(instance=request.user.profile)
+
+  context = {
+    'uForm' : uForm,
+    'pForm' : pForm
+  }
+
+  return render(request, 'users/profile.html', context)
